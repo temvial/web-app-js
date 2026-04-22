4f5d427a6fca6de7181f828b27cf0b090199a227
temvial <77891642+temvial@users.noreply.github.com>
2026-04-22 09:23:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('Какой-то текст');
+console.log('Какой-то текст'); ///для 1 лабы
+
+const listEl = document.getElementById('list');
+const messageEl = document.getElementById('message');
+
+const btnAll = document.getElementById('btnAll');
+const btnNew = document.getElementById('btnNew');
+const btnSort = document.getElementById('btnSort');
+const btnStats = document.getElementById('btnStats');
+
+btnAll.addEventListener('click', function() {
+    messageEl.textContent = "";
+    renderList(emps);
+});
+
+btnNew.addEventListener('click', function() {
+    messageEl.textContent = "";
+    renderList(filterByStatus(emps, "new"));
+});
+
+btnSort.addEventListener('click', function() {
+    messageEl.textContent = "";
+    renderList(sortByValueDesc(emps));
+});
+
+btnStats.addEventListener('click', function() {
+    listEl.textContent = "";
+    const stats = buildStats(emps);
+    messageEl.textContent =
+        `Всего записей: ${stats.totalCount}\n` +
+        `Сумма value: ${stats.sumValue}\n` +
+        `Максимум value: ${stats.maxValue}\n` +
+        `Количество status="new": ${stats.newCount}`;
+});
